464770b74feb199c33ef08e640e3dc26253e4e02
Makisuo <minetoaster12@gmail.com>
2021-02-11 22:26:39 +0100

    Add Cache to all endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,52 +1,76 @@
-import { Controller, Get, Res, Param, HttpStatus } from '@nestjs/common'
-import { AppService } from './app.service'
+import {
+  Controller,
+  Get,
+  Res,
+  Param,
+  HttpStatus,
+  UseInterceptors,
+  CacheInterceptor,
+} from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
+@UseInterceptors(CacheInterceptor)
 export class AppController {
-	constructor (private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {}
 
-	//General Backend Controller
-	@Get()
-	getServer (): string {
-		return this.appService.getServer()
-	}
+  //General Backend Controller
+  @Get()
+  getServer(): string {
+    return this.appService.getServer();
+  }
 
-	//Gets summoner data by name
-	@Get('summoner/by-name/:name')
-	async getSummonerByName (@Res() res, @Param('name') name: string): Promise<JSON> {
-		const response = await this.appService.getSummonerByName(name)
-		return res.status(HttpStatus.OK).json(response)
-	}
+  //Gets summoner data by name
+  @Get('summoner/by-name/:name')
+  async getSummonerByName(
+    @Res() res,
+    @Param('name') name: string,
+  ): Promise<JSON> {
+    const response = await this.appService.getSummonerByName(name);
+    return res.status(HttpStatus.OK).json(response);
+  }
 
-	//Gets mastery data by name
-	@Get('mastery/by-name/:name')
-	async getMasteryDataBySummonerName (@Res() res, @Param('name') name: string): Promise<JSON> {
-		const response = await this.appService.getMasteryDataBySummonerName(name)
-		return res.status(HttpStatus.OK).json(response)
-	}
+  //Gets mastery data by name
+  @Get('mastery/by-name/:name')
+  async getMasteryDataBySummonerName(
+    @Res() res,
+    @Param('name') name: string,
+  ): Promise<JSON> {
+    const response = await this.appService.getMasteryDataBySummonerName(name);
+    return res.status(HttpStatus.OK).json(response);
+  }
 
-	//Gets champion mastery data by champion name of user
-	@Get('mastery/by-champ/:champion/by-name/:username/')
-	async getMasteryDataBySummonerNameByChampion (
-		@Res() res,
-		@Param('username') username: string,
-		@Param('champion') champion: string,
-	): Promise<JSON> {
-		const response = await this.appService.getMasteryDataOfChampionBySummonerName(username, champion)
-		return res.status(HttpStatus.OK).json(response)
-	}
+  //Gets champion mastery data by champion name of user
+  @Get('mastery/by-champ/:champion/by-name/:username/')
+  async getMasteryDataBySummonerNameByChampion(
+    @Res() res,
+    @Param('username') username: string,
+    @Param('champion') champion: string,
+  ): Promise<JSON> {
+    const response = await this.appService.getMasteryDataOfChampionBySummonerName(
+      username,
+      champion,
+    );
+    return res.status(HttpStatus.OK).json(response);
+  }
 
-	//Gets lowest mastery score of user by name
-	@Get('mastery/lowest/by-name/:name')
-	async getLowestMasteryDataBySummonerName (@Res() res, @Param('name') name: string): Promise<JSON> {
-		const response = await this.appService.getLowestChampionMastery(name)
-		return res.status(HttpStatus.OK).json(response)
-	}
+  //Gets lowest mastery score of user by name
+  @Get('mastery/lowest/by-name/:name')
+  async getLowestMasteryDataBySummonerName(
+    @Res() res,
+    @Param('name') name: string,
+  ): Promise<JSON> {
+    const response = await this.appService.getLowestChampionMastery(name);
+    return res.status(HttpStatus.OK).json(response);
+  }
 
-	//Get next mastery level up of user by name
-	@Get('mastery/next/by-name/:name')
-	async getNextMasteryDataBySummonerName (@Res() res, @Param('name') name: string): Promise<JSON> {
-		const response = await this.appService.getNextChampionLevelUp(name)
-		return res.status(HttpStatus.OK).json(response)
-	}
+  //Get next mastery level up of user by name
+  @Get('mastery/next/by-name/:name')
+  async getNextMasteryDataBySummonerName(
+    @Res() res,
+    @Param('name') name: string,
+  ): Promise<JSON> {
+    const response = await this.appService.getNextChampionLevelUp(name);
+    return res.status(HttpStatus.OK).json(response);
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
-import { Module, HttpModule } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { ConfigModule } from '@nestjs/config'
-import { ScrapingModule } from './scraping/scraping.module'
+import { Module, HttpModule, CacheModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ScrapingModule } from './scraping/scraping.module';
 
 @Module({
-	imports: [ ConfigModule.forRoot(), HttpModule, ScrapingModule ],
-	controllers: [ AppController ],
-	providers: [ AppService ],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      ttl: 600,
+      max: 150,
+    }),
+    HttpModule,
+    ScrapingModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
